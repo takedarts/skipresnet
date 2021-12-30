773f3953f983aa196b302f686d71062ec86d66c8
Atsushi TAKEDA <atsushi@takedarts.jp>
2021-12-30 13:45:15 +0900

    fix: Update the implementation of stochastic depth to run on TPUs.
    In the previous version, the forward graph has been changed for stochastic depth,
    but an enormous overhead on TPUs is caused by the procedure.
    In the new implementation, codes of stochastic depth no longer changes the forward graph.

--- src/models/modules/stochastic_depth.py
+++ src/models/modules/stochastic_depth.py
@@ -1,5 +1,5 @@
+import torch
 import torch.nn as nn
-import numpy as np
 
 
 class StochasticDepth(nn.Module):
@@ -12,9 +12,9 @@ class StochasticDepth(nn.Module):
         if not self.training or self.drop_prob == 0.0:
             return x
         else:
-            d = float(np.random.rand() >= self.drop_prob)
+            m = (torch.rand_like(x[:, 0, 0, 0]) >= self.drop_prob).float()
             r = 1.0 - self.drop_prob
-            return x * d / r
+            return x * m[:, None, None, None] / r
 
     def extra_repr(self):
         return 'drop={}'.format(self.drop_prob)
