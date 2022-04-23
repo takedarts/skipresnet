b0808cedf2efcadc37321ac60edb8cc4949645ee
Atsushi TAKEDA <atsushi@takedarts.jp>
2022-04-23 15:45:30 +0900

    style: Change file names.

--- src/models/modules/__init__.py
+++ src/models/modules/__init__.py
@@ -1,16 +1,16 @@
-from .blurpool import BlurPool2d  # noqa
-from .channelpad import ChannelPad  # noqa
+from .blur_pool import BlurPool2d  # noqa
+from .channel_pad import ChannelPad  # noqa
 from .conv2d_same import Conv2dSame  # noqa
-from .dropblock import DropBlock  # noqa
-from .layernorm import LayerNorm2d  # noqa
+from .drop_block import DropBlock  # noqa
+from .inplace_norm import InplaceNorm  # noqa
+from .layer_norm import LayerNorm2d  # noqa
 from .multiply import Multiply  # noqa
 from .reshape import Reshape  # noqa
-from .semodule import SEModule  # noqa
 from .shakedrop import ShakeDrop  # noqa
 from .sigmoid import HSigmoid  # noqa
 from .signal_augmentation import SignalAugmentation  # noqa
 from .split_attention import SplitAttentionModule  # noqa
-from .stdconv import ScaledStdConv2d, ScaledStdConv2dSame  # noqa
+from .squeeze_excite import SEModule  # noqa
+from .std_conv import ScaledStdConv2d, ScaledStdConv2dSame  # noqa
 from .stochastic_depth import StochasticDepth  # noqa
 from .swish import HSwish, Swish  # noqa
-from .inplace_norm import InplaceNorm  # noqa
