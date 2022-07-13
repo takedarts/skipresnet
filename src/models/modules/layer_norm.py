f63546a07c2e8ac8e8e47ae6ae84c21da45181bd
Atsushi TAKEDA <atsushi@takedarts.jp>
2022-07-13 21:17:37 +0900

    Change the calculation of `LayerNorm2d` for stabilization. The calculation of `LayerNorm2d` is changed to the python code from the native code of PyTorch standard library. The native code of LayerNorm causes unstable training in some downstream tasks.

--- src/models/modules/layer_norm.py
+++ src/models/modules/layer_norm.py
@@ -6,13 +6,9 @@ class LayerNorm2d(nn.LayerNorm):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         assert x.dim() == 4, 'Shape of input tensor must be (B, C, H, W)'
 
-        if x.is_contiguous():
-            x = x.permute(0, 2, 3, 1)
-            x = super().forward(x)
-            x = x.permute(0, 3, 1, 2)
-        else:
-            s, u = torch.var_mean(x, dim=1, unbiased=False, keepdim=True)
-            x = (x - u) * torch.rsqrt(s + self.eps)
+        s, u = torch.var_mean(x, dim=1, unbiased=False, keepdim=True)
+        x = (x - u) * torch.rsqrt(s + self.eps)
+        if self.weight is not None:
             x = x * self.weight[:, None, None] + self.bias[:, None, None]
 
         return x
